1bf462dd4c8733fd8ea8c7f7e3d8649338a70853
Jeferson Klein <adelainegelain@gmail.com>
2017-02-11 23:40:42 -0200

    rebulding

---
+++ String.Extensions.Test/src/StringExtensionsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomExtensions;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class StringExtensionsTest
+    {
+        [TestMethod]
+        public void TestLowerCase()
+        {
+            const string expectedValue = "pneumnultramicrcpicilicvlcancnii";
+
+            var stringTested = "4/pneumonoultramicroscopicsilicovolcanoconiosis-1_()";
+            stringTested = stringTested.Remove("(", ")", "1", "-", "/", "4", "o", "s", "_");
+
+            Assert.AreEqual(expectedValue, stringTested);
+        }
+
+        [TestMethod]
+        public void TestUpperCase()
+        {
+            const string expectedValue = "PNEUMNULTRAMICRCPICILICVLCANCNII";
+
+            var stringTested = "4/PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS-1_()";
+            stringTested = stringTested.Remove(true, "(", ")", "1", "-", "/", "4", "o", "s", "_");
+
+            Assert.AreEqual(expectedValue, stringTested);
+        }
+    }
+}
---
+++ String.Extensions/src/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CustomExtensions
+{
+    public static class StringExtensions
+    {
+        public static string Remove(this string str, params string[] toRemove)
+        {
+            return RemoveStringConsiderCase(str, toRemove);
+        }
+
+        public static string Remove(this string str, bool ignoreCase, params string[] toRemove)
+        {
+            if (ignoreCase)
+                return RemoveStringIgnoreCase(str, toRemove);
+            else
+                return RemoveStringConsiderCase(str, toRemove);
+        }
+
+        private static string RemoveStringConsiderCase(this string value, string[] toRemove)
+        {
+            foreach (var item in toRemove)
+                value = value.Replace(item, string.Empty);
+
+            return value;
+        }
+
+        private static string RemoveStringIgnoreCase(this string value, string[] toRemove)
+        {
+            foreach (var item in toRemove)
+                value = Regex.Replace(value, Regex.Escape(item), string.Empty, RegexOptions.IgnoreCase);
+
+            return value;
+        }    
+        
+    }
+}
