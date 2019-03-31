e330315a3472ccf265b256bb4f8d6201b6919c23
Ivan Stoychev <ivanstoychev44@gmail.com>
2019-03-31 16:33:10 +0300

    Add "Substring" method that returns a substring of the instance string, located between two given strings.

--- String.Extensions.Test/src/ReplacerTest.cs
+++ String.Extensions.Test/src/ReplacerTest.cs
@@ -26,5 +26,49 @@ namespace String.Extensions.Test.src
 
             Assert.AreEqual(expectedValue, result);
         }
+
+        [TestMethod]
+        public void Substring_IncludeStart_ExcludeEnd()
+        {
+            const string testString = "This is a test string";
+            const string expectedValue = "is a test ";
+
+            string result = testString.Substring("is", true, "string", false);
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void Substring_IncludeStart_IncludeEnd()
+        {
+            const string testString = "This is a test string";
+            const string expectedValue = "is a test";
+
+            string result = testString.Substring("is", true, "test", true);
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void Substring_ExcludeStart_ExcludeEnd()
+        {
+            const string testString = "This is a test string";
+            const string expectedValue = " a test ";
+
+            string result = testString.Substring("is", false, "string", false);
+
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void Substring_ExcludeStart_IncludeEnd()
+        {
+            const string testString = "This is a test string";
+            const string expectedValue = " a test string";
+
+            string result = testString.Substring("is", false, "string", true);
+
+            Assert.AreEqual(expectedValue, result);
+        }
     }
 }
--- String.Extensions/src/Replacer.cs
+++ String.Extensions/src/Replacer.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace String.Extensions
 {
@@ -10,12 +11,30 @@ namespace String.Extensions
         /// <param name="ignoreCase">Whether the replacement should be case sensetive or not.</param>
         /// <param name="newString">The string to replace all occurances of oldStrings</param>
         /// <param name="oldStrings">Array of strings to be replaced.</param>
+        /// <returns></returns>
         public static string Replace(this string str, bool ignoreCase, string newString, string[] oldStrings)
         {
             if (ignoreCase)
                 return ReplaceStringIgnoreCase(str, newString, oldStrings);
             else
                 return ReplaceStringConsiderCase(str, newString, oldStrings);
+        }
+
+        /// <summary>
+        /// Retrieves the substring located between two given strings from this instance.
+        /// </summary>
+        /// <param name="str">The instance from which to extract a substring.</param>
+        /// <param name="startString">The string which marks the start of the substring.</param>
+        /// <param name="includeStartString">A boolean indicating whether the substring should include the given startString.</param>
+        /// <param name="endString">The string which marks the end of the substring.</param>
+        /// <param name="includeEndString">A boolean indicating whether the substring should include the given endString.</param>
+        /// <returns>A string representing the part of the original string, located between the startString and endString.</returns>
+        public static string Substring(this string str, string startString, bool includeStartString, string endString, bool includeEndString)
+        {
+            int startStringIndex = str.IndexOf(startString) + startString.Length * Convert.ToInt32(!includeStartString);
+            int endStringIndex = str.IndexOf(endString) + endString.Length * Convert.ToInt32(includeEndString);
+
+            return str.Substring(startStringIndex, endStringIndex);
         }
 
         private static string ReplaceStringConsiderCase(this string value, string newString, string[] oldStrings)
