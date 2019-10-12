ee0cdeb01c24cbbb7e0caf081b78576502780cb3
Ivan Stoychev <ivanstoychev44@gmail.com>
2019-10-12 12:51:02 +0300

    Improved "Remover" summaries and "RemoverTest" methods by making them Theories, instead of Facts.

--- String.Extensions.Test/src/RemoverTest.cs
+++ String.Extensions.Test/src/RemoverTest.cs
@@ -1,78 +1,62 @@
 ﻿using Xunit;
-using String.Extensions;
 
-namespace UnitTestProject1
+namespace String.Extensions.Test.src
 {
     public class RemoverTest
     {
         [Theory]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        public void RemoveStringConsiderCase(string testString, string expected)
+        [InlineData("Lorem lorem IPSUM ipsum", "Lorem   ipsum")]
+        [InlineData("lorem LORem ipSUM IPSUM", " LORem ipSUM ")]
+        [InlineData("lorem ipsum IPSUM", " ipsum ")]
+        public void RemoveString_Test_ConsiderCase(string testString, string expected)
         {
-            const string expectedValue = "pneumnultramicrcpicilicvlcancnii";
+            string actual = testString.Remove("lorem", "IPSUM");
 
-            var stringTested = "4/pneumonoultramicroscopicsilicovolcanoconiosis-1_()";
-            stringTested = stringTested.Remove("(", ")", "1", "-", "/", "4", "o", "s", "_");
-
-            Assert.Equal(expectedValue, stringTested);
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        public void RemoveStringIgnoreCaseTest(string testString, string expected)
+        [InlineData("Lorem lorem IPSUM ipsum", "   ")]
+        [InlineData("lorem LORem ipSUM IPSUM", "   ")]
+        [InlineData("lorem ipsum IPSUM", "  ")]
+        public void RemoveString_Test_IgnoreCase(string testString, string expected)
         {
-            const string expectedValue = "PNEUMNULTRAMICRCPICILICVLCANCNII";
+            string actual = testString.Remove(true, "lorem", "IPSUM");
 
-            var stringTested = "4/PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS-1_()";
-            stringTested = stringTested.Remove(true, "(", ")", "1", "-", "/", "4", "o", "s", "_");
-
-            Assert.Equal(expectedValue, stringTested);
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        public void RemoveAllNumbersTest(string testString, string expected)
+        [InlineData("b2HGLTxiVJz0cxWacceZ", "bHGLTxiVJzcxWacceZ")]
+        [InlineData("S1Rsf3jsE2IPKhIML5kN", "SRsfjsEIPKhIMLkN")]
+        [InlineData("xqyao0g2EwQmweZUqy6l", "xqyaogEwQmweZUqyl")]
+        public void RemoveNumbers_Test(string testString, string expected)
         {
-            const string expectedValue = "February th, ";
+            string actual = testString.RemoveNumbers();
 
-            var stringTested = "February 2th, 1989";
-            stringTested = stringTested.RemoveAllNumbers();
-
-            Assert.Equal(expectedValue, stringTested);
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        public void RemoveAllSpecialCharactersTest(string testString, string expected)
+        [InlineData(@"5riD+p-AvsnZImA@i[(r", @"5riDpAvsnZImAir")]
+        [InlineData(@"qnxON$cEX\=i?]3s3MyB", @"qnxONcEXi3s3MyB")]
+        [InlineData(@"^{b,H:b{ft^3TU<PZJGd", @"bHbft3TUPZJGd")]
+        public void RemoveSpecialCharacters_Test(string testString, string expected)
         {
-            const string expectedValue = "711worksweekendsSatandSun";
+            string actual = testString.RemoveSpecialCharacters();
 
-            var stringTested = "7-11 works_weekends (Sat and Sun)";
-            stringTested = stringTested.RemoveAllSpecialCharacters();
-
-            Assert.Equal(expectedValue, stringTested);
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        [InlineData("", "")]
-        public void RemoveAllAlphabeticalLettersTest(string testString, string expected)
+        [InlineData(@"rMhj5(VirYg<rS7lWuGZ", @"5(<7")]
+        [InlineData(@"gNa/*+Kd($@k+$%8Fj/I", @"/*+($@+$%8/")]
+        [InlineData(@"bJcnmhy7<9-h%]wk[dTc", @"7<9-%][")]
+        public void RemoveLetters_Test(string testString, string expected)
         {
-            const string expectedValue = "__ - ()";
+            string actual = testString.RemoveLetters();
 
-            var stringTested = "_asd_ a-b (owo)";
-            stringTested = stringTested.RemoveAllAlphabeticalLetters();
-
-            Assert.Equal(expectedValue, stringTested);
+            Assert.Equal(expected, actual);
         }
     }
 }
